a85872059311e7948e9612c9ded9f890b2328e27
Sagar Upadhyay <sagarupadhyay106@gmail.com>
2026-02-14 23:58:43 +0530

    adding signup sign in toggle

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import Header from "../../components/Header";
 import { bgImage } from "../../constants/constants";
 
 const Login = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+
+  const toggleSignin = () => {
+    setIsSignIn(!isSignIn);
+  };
+
   return (
     <div
       className="min-h-screen bg-cover bg-center relative"
@@ -12,7 +19,9 @@ const Login = () => {
         <Header />
         <div className="min-h-screen flex items-center justify-center text-center">
           <form className="w-4/12 bg-black/60 flex flex-col gap-4 p-8 rounded">
-            <h1 className="text-white text-2xl font-semibold pb-5">Login</h1>
+            <h1 className="text-white text-2xl font-semibold pb-5">
+              {isSignIn ? "Sign In" : "Sign Up"}
+            </h1>
 
             <input
               type="text"
@@ -29,6 +38,11 @@ const Login = () => {
             <button className="bg-[#a80000] text-white py-3 rounded font-semibold cursor-pointer hover:bg-[#348fdc]">
               Sign In
             </button>
+            <p className="text-white cursor-pointer" onClick={toggleSignin}>
+              {isSignIn
+                ? "New to Cineflix? Sign Up"
+                : "Already a user? Sign In"}
+            </p>
           </form>
         </div>
       </div>
